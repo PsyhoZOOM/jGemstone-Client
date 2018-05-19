20d1b28ebb9b947d2f75174d4075b3463dd0c08a
PsyhoZOOM <PsyhoZOOM@gmail.com>
2018-05-19 18:16:07 +0200

    value of perc perc value of value

--- src/main/java/net/yuvideo/jgemstone/client/classes/valueToPercent.java
+++ src/main/java/net/yuvideo/jgemstone/client/classes/valueToPercent.java
@@ -6,6 +6,17 @@ import java.text.DecimalFormat;
  * Created by zoom on 1/23/17.
  */
 public class valueToPercent {
+    public static Double getPDVOfValue(double value, double perc){
+        double pdv = value * perc / 100;
+        return  pdv;
+    }
+
+    public static Double getPDVOfSum(double value, double perc){
+        double pdv = (value * perc) / (100 + perc);
+        return pdv ;
+    }
+
+
 
 
 
