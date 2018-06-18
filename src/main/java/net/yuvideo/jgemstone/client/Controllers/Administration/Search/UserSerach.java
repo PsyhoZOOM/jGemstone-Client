0b35748eac5afcee1e059475b69a74bf181006da
PsyhoZOOM <psyhozoom@gmail.com>
2018-06-18 14:15:21 +0200

    Init

---
+++ src/main/java/net/yuvideo/jgemstone/client/Controllers/Administration/Search/UserSerach.java
@@ -0,0 +1,18 @@
+package net.yuvideo.jgemstone.client.Controllers.Administration.Search;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class UserSerach implements Initializable {
+
+
+  private URL location;
+  private ResourceBundle resources;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    this.location = location;
+    this.resources = resources;
+  }
+}
